1c52f9b5dfde8217645b89e624afd5a2ff6b080d
Nastia <fomina.nst@gmail.com>
2018-11-12 18:05:47 +0300

    correcting

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
   def self.title(category_title)
-    self.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category_title).order('tests.title DESC').pluck(:title)
+    Test.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category_title).order('tests.title DESC').pluck(:title)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20181111222104) do
     t.integer "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_test_passages_on_test_id"
+    t.index ["user_id"], name: "index_test_passages_on_user_id"
   end
 
   create_table "tests", force: :cascade do |t|
