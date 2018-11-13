03aed349388bbc8f602e7a4f02edb876a71f2437
Nastia <fomina.nst@gmail.com>
2018-11-14 00:28:48 +0300

    change

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
   def self.title(category_title)
     joins('JOIN categories ON tests.category_id = categories.id')
-      .where('categories.title = ?', category_title)
-      .order('tests.title DESC')
+      .where(categories: { title: category_title })
+      .order(title: :desc)
       .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   def tests_passage(level)
     Test.joins('JOIN test_passage ON tests_id = test.passages.id')
-        .where('test_passages.user_id = ?', id)
-        .where('tests.level = ?', level)
+        .where(test_passages: {user_id: id })
+        .where(level: level)
   end
 end
