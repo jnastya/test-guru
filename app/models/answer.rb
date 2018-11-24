ffbe6bf01d9a71e81b49586077eeb81e9377d942
Nastya <fomina.nst@gmail.com>
2018-11-25 01:54:10 +0300

    pr fix

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,13 +3,13 @@ class Answer < ApplicationRecord
 
   validates :body, presence: true
 
-  validate :validate_amount_answers
+  validate :validate_amount_answers, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_amount_answers
-    errors.add(:question, 'Превышено количество допустимых ответов') if question.answers.size >= 4, on: :create
+    errors.add(:question, 'Превышено количество допустимых ответов') if question.answers.size >= 4
   end
 end
