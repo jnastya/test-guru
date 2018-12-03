50c45ff2d60de2d030440a9b336c7b8ecb66aece
Nastia <fomina.nst@gmail.com>
2018-12-03 12:57:25 +0300

    views and helpers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,33 +1,43 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_question, only: %i[show destroy]
-  before_action :find_test, only: %i[index create]
+  before_action :find_question, only: %i[edit show update destroy]
+  before_action :find_test, only: %i[index create new]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    render json: { questions: Question.all }
-  end
-
-  def show
-    render inline: '<%= @question.body %>'
-  end
-
-  def new
-
+    @questions = @test.questions
   end
 
   def create
     question = @test.questions.new(question_params)
     if question.save
-      render plain: question.inspect
+      redirect_to test_questions_path(@test)
     else
       render :new
     end
   end
 
+  def new
+    @question = @test.questions.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @question.destroy
-    render plain: 'Question deleted'
+    redirect_to test_questions_path(@question.test_id)
   end
 
   private
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,29 +1,55 @@
 class TestsController < ApplicationController
 
+  before_action :find_test, only: %i[edit show update destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
   def index
-    render json: { tests: Test.all }
-  end
-
-  def new
-  end
-
-  def show
-    @test = Test.find(params[:id])
-    render inline: '<%= @test.title %>'
+    @tests = Test.all
   end
 
   def create
-    @test = Test.create(test_params)
+    @test = Test.new(test_params)
     if @test.save
-      redirect_to root_path
+      redirect_to tests_path
     else
       render :new
     end
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to tests_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
   end
 
   private
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def rescue_with_test_not_found
+    render plain: 'Test was not found'
   end
 end
