8be959be47d0c6eeb597688c9f3cad35bea06903
Nastia <fomina.nst@gmail.com>
2018-12-08 01:39:17 +0300

    correcting

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,8 @@
 class QuestionsController < ApplicationController
 
   before_action :find_question, only: %i[edit show update destroy]
-  before_action :find_test, only: %i[index create new]
+  before_action :find_test, only: %i[create new]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-
-  def index
-    @questions = @test.questions
-  end
 
   def create
     question = @test.questions.new(question_params)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def current_year
-    Time.now.year
+    Time.current.year
   end
 
   def github_url(author, repo)
