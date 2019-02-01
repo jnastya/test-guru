da8ea5df0f9072f89ac739c67b4444f0d05ebd7a
Nastia <fomina.nst@gmail.com>
2019-02-01 18:40:47 +0300

    correction

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'rails', '~> 5.2.1'
 
 gem 'pg'
 
+gem 'dotenv-rails'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -23,11 +23,12 @@ class TestPassagesController < ApplicationController
   def gist
     result = GistQuestionService.new(@test_passage.current_question).call
 
-    flash_options = if result.html_url.present?
-      current_user.gists.create(question: @test_passage.current_question, gist_url: result.html_url)
-      { notice: t('.success', gist_url: result.html_url) }
-    else
-      { alert: t('.failure') }
+    flash_options =
+      if result.html_url.present?
+        current_user.gists.create!(question: @test_passage.current_question, gist_url: result.html_url)
+        { notice: t('.success', gist_url: result.html_url) }
+      else
+        { alert: t('.failure') }
     end
 
     redirect_to @test_passage, flash_options
